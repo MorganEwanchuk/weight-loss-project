fe6a70559223e5d770fc19d68fe2eed5de5854a1
Morgan <mewanchuk@live.com>
2023-02-18 14:50:11 -0700

    added logout routes and ensureGuest middleware

--- routes/main.js
+++ routes/main.js
@@ -3,11 +3,16 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
 
+const {ensureAuth,ensureGuest} = require('../middleware/auth')
 
 
-router.get('/', homeController.renderHome)
-router.get('/login', authController.getLogin)
+router.get('/', ensureGuest,(req,res) => {
+    res.render('index')
+})
+router.get('/login', ensureGuest, authController.getLogin)
+router.post('/login', authController.postLogin)
 
+router.get('/logout', authController.logout)
 
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
