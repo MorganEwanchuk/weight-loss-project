0ad7e631a227d0e2667fd4ed01a37036d17a2ef3
Morgan <mewanchuk@live.com>
2023-02-18 14:50:48 -0700

    added logout route on main page

--- routes/weight.js
+++ routes/weight.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const weightController = require('../controllers/addweight')
-
+const authController = require('../controllers/auth')
 
 router.get('/', weightController.renderAddWeight)
+router.get('/logout', authController.logout)
+
+
 
 module.exports = router
