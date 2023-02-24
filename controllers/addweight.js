7fa6660621f74931a2d7aa770a3567fc3f58ee82
Morgan <mewanchuk@live.com>
2023-02-23 17:58:55 -0700

    added addProfile function to controller

--- controllers/addweight.js
+++ controllers/addweight.js
@@ -1,7 +1,26 @@
-module.exports = {
-    
-    renderAddWeight: (req,res) =>{
-        res.render('addweight')
-    }
+const Profile = require('../models/Profile')
 
+
+
+exports.renderAddWeight = (req,res) => {
+    
+   
+        res.render('addweight')
+    
+
+}
+
+exports.addProfile = (req,res,next) =>{
+    const profile = new Profile({
+        initialWeight: req.body.initialWeight,
+        weightLostPerWeek: req.body.weightLostPerWeek,
+        goalWeight: req.body.goalWeight
+      })
+      profile.save((err) => {
+        if (err) { return next(err) }
+        
+        res.redirect(req.session.returnTo || '/')
+
+        
+      })
 }
