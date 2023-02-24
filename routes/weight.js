65dedb3f92fa7f369120ea8a7ed79fe901ba924e
Morgan <mewanchuk@live.com>
2023-02-23 17:59:24 -0700

    added addProfile route

--- routes/weight.js
+++ routes/weight.js
@@ -3,7 +3,13 @@ const router = express.Router()
 const weightController = require('../controllers/addweight')
 const authController = require('../controllers/auth')
 
+
+
+
 router.get('/', weightController.renderAddWeight)
+
+router.post('/addprofile', weightController.addProfile)
+
 router.get('/logout', authController.logout)
 
 
