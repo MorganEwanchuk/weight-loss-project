1261a0826e587d6ebaff250309da22e2b15def33
Morgan <mewanchuk@live.com>
2023-02-26 13:19:41 -0700

    added comments on the routes page

--- routes/main.js
+++ routes/main.js
@@ -9,11 +9,15 @@ const {ensureAuth,ensureGuest} = require('../middleware/auth')
 router.get('/', ensureGuest,(req,res) => {
     res.render('index')
 })
+
+// Routes for getting and posting on the login page
 router.get('/login', ensureGuest, authController.getLogin)
 router.post('/login', authController.postLogin)
 
+// Route for getting the logout function
 router.get('/logout', authController.logout)
 
+// Routes for getting and posting on the signup page
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
 
